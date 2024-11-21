9c837ea23e275e931596d3649c366cfc7d974b8c
Mark Thackham <mark.thackham@gmail.com>
2024-11-21 12:05:34 +0000

    Commit

--- src/mt/__init__.py
+++ src/mt/__init__.py
@@ -1,1 +1,13 @@
+"""
+Package mt
+
+This package provides a collection of modules and functions for univariate analysis and metrics.
+
+Modules:
+- univariate: Contains functions for univariate analysis and metrics.
+
+Attributes:
+__version__ (str): The version number of the mt package.
+
+"""
 __version__ = "0.0.1"
--- src/mt/univariate.py
+++ src/mt/univariate.py
@@ -1,3 +1,16 @@
+"""
+Module: mt.univariate
+
+This module contains functions for univariate analysis and metrics.
+
+Functions:
+- gini_coefficient(y_true, y_pred): 
+Calculate the Gini Coefficient from the given true and predicted values.
+
+- add_numbers(num1, num2): 
+Add two numbers.
+"""
+
 from sklearn.metrics import roc_auc_score
 
 def gini_coefficient(y_true, y_pred):
@@ -6,7 +19,8 @@ def gini_coefficient(y_true, y_pred):
 
     The Gini Coefficient is a measure of inequality in a distribution,
     where a Gini Coefficient of 0 indicates perfect equality (everyone has the same value),
-    and a Gini Coefficient of 1 indicates perfect inequality (one person has all the value, and everyone else has none).
+    and a Gini Coefficient of 1 indicates perfect inequality (one person has all the value, 
+    and everyone else has none).
 
     Parameters:
     y_true (array-like): The true values of the target variable.
@@ -17,6 +31,7 @@ def gini_coefficient(y_true, y_pred):
     """
     auc = roc_auc_score(y_true, y_pred)
     gini = float(2 * auc - 1)
+
     return gini
 
 
